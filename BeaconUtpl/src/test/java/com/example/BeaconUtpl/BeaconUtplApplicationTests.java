c02e3cee18fb7ca4aa83db20cf87bd5bae917dd7
bdportilla1 <bryantporti@gmail.com>
2020-04-25 11:41:16 -0500

    subiendo proyecto

---
+++ BeaconUtpl/src/test/java/com/example/BeaconUtpl/BeaconUtplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BeaconUtpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeaconUtplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
